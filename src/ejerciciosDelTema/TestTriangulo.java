b67087a91d003bfd190729386f6d3d13b2b7898f
Alvaro <alvarojimenez95@hotmail.com>
2016-02-11 12:09:37 +0100

    11/02/2016

--- src/ejerciciosDelTema/TestTriangulo.java
+++ src/ejerciciosDelTema/TestTriangulo.java
@@ -3,8 +3,13 @@ package ejerciciosDelTema;
 public class TestTriangulo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			Triangulo t = new Triangulo(1, 2, 2);
+			System.out.println(t);
+		} catch (IlegalTrianguloException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Triangulo no válido");
+		}
 	}
 
 }
--- src/ejerciciosDelTema/Triangulo.java
+++ src/ejerciciosDelTema/Triangulo.java
@@ -1,5 +1,33 @@
 package ejerciciosDelTema;
 
 public class Triangulo {
+	//atributos
+	private int A;
+	private int B;
+	private int C;
+	
+	/**
+	 * @param A
+	 * @param B
+	 * @param C
+	 * @throws IlegalTrianguloException 
+	 */
+	public Triangulo(int A, int B, int C) throws IlegalTrianguloException {
+		if(trianguloValido(A, B, C)){
+			this.A = A;
+			this.B = B;
+			this.C = C;
+		}
+		else throw new IlegalTrianguloException();
+	}
+	
+	//metodo para comprobar si un triangulo es valido
+	public boolean trianguloValido(int A, int B, int C){
+		return A+B>C && A+C>B && B+C>A;
+	}
 
+	@Override
+	public String toString() {
+		return "Triangulo [A=" + A + ", B=" + B + ", C=" + C + "]";
+	}
 }
